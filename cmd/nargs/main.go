b55bbf6a7c6952949f4ec5e58dd9514d53a2ff49
Alex Kohler <akohler@lutron.com>
2019-05-17 15:32:37 -0400

    revert main

--- cmd/nargs/main.go
+++ cmd/nargs/main.go
@@ -1,7 +1,26 @@
-// main.go
 package main
 
-func nakedret() (l int) {
+import (
+	"flag"
+	"log"
+	"os"
 
-	return
+	"github.com/alexkohler/nargs"
+)
+
+func usage() {
+	log.Printf("Usage of %s:\n", os.Args[0])
+	log.Printf("\nnargs [flags] # runs on package in current directory\n")
+	log.Printf("\nnargs [flags] [packages]\n")
+	log.Printf("Flags:\n")
+	flag.PrintDefaults()
+}
+func main() {
+
+	// Remove log timestamp
+	log.SetFlags(0)
+
+	if err := nargs.CheckForUnusedFunctionArgs(flag.Args()); err != nil {
+		log.Println(err)
+	}
 }
